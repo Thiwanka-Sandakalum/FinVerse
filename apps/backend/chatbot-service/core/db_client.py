79c2618accba27dcd38e2c10c1561f7ea92b4d9a
Thiwanka <thiwanka2002sandakalum@gmail.com>
2025-12-28 15:51:27 +0530

    feat(db): add SSL support for product and chat database connections

--- apps/backend/chatbot-service/core/db_client.py
+++ apps/backend/chatbot-service/core/db_client.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 # Product DB
-product_engine = create_engine(getattr(settings, "PRODUCT_DB_URL"), pool_pre_ping=True)
+product_engine = create_engine(getattr(settings, "PRODUCT_DB_URL"), pool_pre_ping=True,connect_args={ "ssl": {"ssl": True}})
 ProductSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=product_engine)
 
 # Chat DB
-chat_engine = create_engine(getattr(settings, "CHAT_DB_URL"), pool_pre_ping=True)
+chat_engine = create_engine(getattr(settings, "CHAT_DB_URL"), pool_pre_ping=True,connect_args={ "ssl": {"ssl": True}})
 ChatSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=chat_engine)
 
 def get_product_db():
